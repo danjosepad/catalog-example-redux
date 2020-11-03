8de9677e2a3669cba119fc232bd638247ef66d5d
daniel.padilha <danielpadilhapessoal@outlook.com>
2020-11-03 13:55:53 -0300

    feat: ADD_PRODUCT_TO_CART action added

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,13 @@
 import { createStore } from 'redux';
+import { ICartState } from './modules/cart/types';
 
 // Project imports
 
 import rootReducer from './modules/rootReducer';
 
+export interface IState {
+  cart: ICartState
+}
 
 const store = createStore(rootReducer);
 
