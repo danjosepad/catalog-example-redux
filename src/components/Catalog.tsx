6f6ca51cd814846f4494dcdf41071079190108e3
daniel.padilha <danielpadilhapessoal@outlook.com>
2020-11-03 14:15:24 -0300

    feat: `<CatalogItem/>` added -refactor: Catalog items logic transferred to CatalogItem

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
 
 // Project imports
+
 import api from '../services/api';
 import { IProduct } from '../store/modules/cart/types';
-import { addProductToCart } from '../store/modules/cart/actions';
+import CatalogItem from './CatalogItem';
 
 const Catalog: React.FC = () => {
-    const dispatch = useDispatch();
     const [catalog, setCatalog] = useState<IProduct[]>([]);
 
     useEffect(() => {
@@ -15,22 +14,13 @@ const Catalog: React.FC = () => {
         setCatalog(response.data);
       })
     }, []);
-    
-    const handleAddProductToCart = (product: IProduct) => {
-      dispatch(addProductToCart(product))
-    }
 
     return (
       <main>
         <h1>Catalog</h1>
 
         {catalog.map(product => (
-          <article key={product.id}>
-            <strong>{product.title}</strong> {" - "}
-            <span>{product.price}</span> {" "}
-
-            <button type="button" onClick={() => handleAddProductToCart(product)}>Comprar</button>
-          </article>
+          <CatalogItem key={product.id} product={product} />
         ))}
       </main>
     )
