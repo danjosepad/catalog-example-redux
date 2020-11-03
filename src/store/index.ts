0ec17056639261cf4bf674103eee7bcf1047cc65
daniel.padilha <danielpadilhapessoal@outlook.com>
2020-11-03 11:58:55 -0300

    feat: added cart reducers and actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,10 @@
 import { createStore } from 'redux';
 
-const store = createStore(() => {
-  return {
-    name: 'Daniel',
-    email: 'test'
-  }
-})
+// Project imports
+
+import rootReducer from './modules/rootReducer';
+
+
+const store = createStore(rootReducer);
 
 export default store;
---
+++ src/store/modules/rootReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+// Project imports
+import cart from './cart/reducer';
+
+export default combineReducers({
+  cart
+})
